f40fcbb9d7a2f05a400cfcef2da0bc1411acfcfb
Bartek Małachowski <bartek.malachowski@gmail.com>
2020-11-26 22:23:51 +0100

    first commit

---
+++ src/test/java/com/example/dummy/DummyApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.dummy;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyApplicationTests {
+
+    @Disabled
+    @Test
+    void contextLoads() {
+    }
+
+}
